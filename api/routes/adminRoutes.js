b931c9ac533237f097314ef9d9854243a57ef185
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-02 22:06:56 +0500

    Course Routes added

--- api/routes/adminRoutes.js
+++ api/routes/adminRoutes.js
@@ -2,5 +2,6 @@ const adminRouter = require('express').Router();
 
 adminRouter.use('/student', require('./adminRoutes/adminStudentRoutes'));
 adminRouter.use('/teacher', require('./adminRoutes/adminTeacherRoutes'));
+adminRouter.use('/course', require('./adminRoutes/adminCourseRoutes'));
 
 module.exports = adminRouter;
---
+++ api/routes/adminRoutes/adminCourseRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { admin_courses_get_all, admin_course_post, admin_course_get_single, admin_course_delete, admin_course_put } = require('../../controllers/adminControllers/adminCourseControllers');
+const { admin_course_post_validation, admin_course_get_single_validation, admin_course_put_validation } = require('../../validations/adminValidations/adminCourseValidations');
+
+
+const bcrypt = require('bcrypt');
+const Course = require('../../models/Course');
+
+
+// router.get('/', admin_courses_get_all);
+// router.get('/:courseId', admin_course_get_single_validation, admin_course_get_single);
+// router.post('/', admin_course_post_validation, admin_course_post);
+// router.delete('/:courseId', admin_course_delete);
+// router.put('/:courseId', admin_course_put_validation, admin_course_put);
+
+
+// This is a temporary route to add multiple courses to the database
+// router.post("/temp", (req, res) => {
+//   let addCourse = async (course) => {
+//     course.courseId = course.courseId.toUpperCase();
+//     let hashedPassword = await bcrypt.hash(course.password, 10);
+//     course = new Course({ ...course, password: hashedPassword });
+//     await course.save();
+//   }
+//   req.body.forEach(course => {
+//     addCourse(course);
+//   });
+//   res.status(200).json({ message: "Courses added successfully." });
+// })
+// This is a temporary route to add multiple courses to the database
+
+
+module.exports = router;
