10358054eff419f359e4b668237916efa0f5f915
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-05 23:55:35 +0500

    Admin signup completed

--- api/controllers/authControllers/authAdminControllers.js
+++ api/controllers/authControllers/authAdminControllers.js
@@ -2,8 +2,18 @@ const Admin = require("../../models/Admin");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+// This is basic admin signup and is not provided on the UI
 const auth_admin_signup = async (req, res) => {
-  res.send(req.body);
+  // Hashing the password
+  try {
+    const hashedPassword = await bcrypt.hash(req.body["password"], 10);
+    const admin = new Admin({ ...req.body, password: hashedPassword });
+    await admin.save();
+    res.status(200).json({ message: "Admin added successfully." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 }
 
 module.exports = { auth_admin_signup };
