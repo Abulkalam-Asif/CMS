b141380317a8aa08f2650dd0c144089d74ffe328
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-05 22:54:44 +0500

    Admin Model added

--- client/src/components/DataInput.jsx
+++ client/src/components/DataInput.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faExclamation,
   faExclamationCircle,
-  faExclamationTriangle,
-  faInfo,
 } from "@fortawesome/free-solid-svg-icons";
 
 const DataInput = (props) => {
