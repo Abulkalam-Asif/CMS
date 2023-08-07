fcf6631f628d6079856fc85e6231c04f74e83f2f
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-08 00:24:52 +0500

    missing controllers and validations added

--- api/controllers.js
+++ api/controllers.js
@@ -1,6 +1,9 @@
 const authAdminControllers = require("./controllers/authControllers/authAdminControllers");
 const authStudentControllers = require("./controllers/authControllers/authStudentControllers");
+const authTeacherControllers = require("./controllers/authControllers/authTeacherControllers");
 const adminStudentControllers = require("./controllers/adminControllers/adminStudentControllers");
+const adminTeacherControllers = require("./controllers/adminControllers/adminTeacherControllers");
+const adminCourseControllers = require("./controllers/adminControllers/adminCourseControllers");
 
 
-module.exports = { ...authAdminControllers, ...adminStudentControllers, ...authStudentControllers };
+module.exports = { ...authAdminControllers, ...authStudentControllers, ...authTeacherControllers, ...adminStudentControllers, ...adminTeacherControllers, ...adminCourseControllers };
--- api/validations.js
+++ api/validations.js
@@ -1,5 +1,8 @@
 const authAdminValidations = require("./validations/authValidations/authAdminValidations");
 const authStudentValidations = require("./validations/authValidations/authStudentValidations");
+const authTeacherValidations = require("./validations/authValidations/authTeacherValidations");
 const adminStudentValidations = require("./validations/adminValidations/adminStudentValidations");
+const adminTeacherValidations = require("./validations/adminValidations/adminTeacherValidations");
+const adminCourseValidations = require("./validations/adminValidations/adminCourseValidations");
 
-module.exports = { ...authAdminValidations, ...adminStudentValidations, ...authStudentValidations }
+module.exports = { ...authAdminValidations, ...adminStudentValidations, ...authTeacherValidations, ...authStudentValidations, ...adminTeacherValidations, adminCourseValidations }
