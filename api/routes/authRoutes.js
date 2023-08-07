57576ac1272740dce8f700b1156e3f147c038b7e
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-08 01:26:25 +0500

    authTeacherRoutes file added to authRoutes

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -2,5 +2,6 @@ const authRouter = require('express').Router();
 
 authRouter.use('/admin', require('./authRoutes/authAdminRoutes'));
 authRouter.use('/student', require('./authRoutes/authStudentRoutes'));
+authRouter.use('/teacher', require('./authRoutes/authTeacherRoutes'));
 
 module.exports = authRouter;
