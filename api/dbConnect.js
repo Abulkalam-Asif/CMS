43b75ebf949a2f26561cb64db378479cc4186279
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-09 16:55:59 +0500

    minor changes

--- api/dbConnect.js
+++ api/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL2);
+    await mongoose.connect(process.env.DB_URL_ATLAS);
     console.log('Successfully connected to MongoDB');
   } catch (error) {
     console.log('Connection to MongoDB Failed!');
