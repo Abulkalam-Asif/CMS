8c015aa23d50648859b55d6d12078c02d39ec48e
Abulkalam-Asif <abulkalamasif1129@gmail.com>
2023-08-15 00:14:58 +0500

    Buttons made responsive

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -4,16 +4,17 @@ const Button = (props) => {
   const { content, onClick, className, size, customAttributes } = props;
   let sizeStyles;
   if (size === "small") {
-    sizeStyles = "p-2 font-semibold";
+    sizeStyles = "p-2 font-semibold border-2";
   } else if (size === "medium") {
-    sizeStyles = "py-2 px-4 text-xl font-semibold";
+    sizeStyles = "py-2 px-4 font-semibold border-2 text-xl";
   } else if (size === "large") {
-    sizeStyles = "py-3 px-6 text-3xl font-bold";
+    sizeStyles =
+    "py-3 px-6 font-bold border-4 text-3xl lg:text-2xl md:text-xl md:py-2";
   }
   return (
     <>
       <button
-        className={`${className} border-2 border-pink-700 text-pink-700 rounded-lg hover:bg-pink-700 hover:text-white transition-colors duration-200 ${sizeStyles}`}
+        className={`${className} text-center border-2 border-pink-700 text-pink-700 rounded-lg hover:bg-pink-700 hover:text-white transition-colors duration-200 ${sizeStyles}`}
         onClick={onClick}
         {...customAttributes}>
         {content}
--- client/src/components/LinkButton.jsx
+++ client/src/components/LinkButton.jsx
@@ -7,15 +7,16 @@ const LinkButton = (props) => {
   if (size === "small") {
     sizeStyles = "p-2 font-semibold border-2";
   } else if (size === "medium") {
-    sizeStyles = "py-2 px-4 text-xl font-semibold border-2";
+    sizeStyles = "py-2 px-4 font-semibold border-2 text-xl";
   } else if (size === "large") {
-    sizeStyles = "py-3 px-6 text-3xl font-bold border-4";
+    sizeStyles =
+      "py-3 px-6 font-bold border-4 text-3xl lg:text-2xl md:text-xl md:py-2";
   }
   return (
     <>
       <Link
         to={to}
-        className={`${className} border-pink-700 text-pink-700 rounded-lg hover:bg-pink-700 hover:text-white transition-colors duration-200 ${sizeStyles}`}
+        className={`${className} text-center border-pink-700 text-pink-700 rounded-lg hover:bg-pink-700 hover:text-white transition-colors duration-200 ${sizeStyles}`}
         onClick={onClick}>
         {content}
       </Link>
